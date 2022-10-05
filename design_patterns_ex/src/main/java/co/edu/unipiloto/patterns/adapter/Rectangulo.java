cd6316077c0ac2675e6022e790232305d3deb0b7
gilberto-pedraza <gilberto-pedraza@B-SG702-21.upiloto.edu>
2022-10-05 14:54:04 -0500

    Creacion proyecto 2022_2 curso Patrones de diseno

---
+++ design_patterns_ex/src/main/java/co/edu/unipiloto/patterns/adapter/Rectangulo.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.unipiloto.patterns.adapter;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Rectangulo {
+    // atributos largo y ancho
+    private double largo;
+    private double ancho;
+    
+    //metodos get y set
+
+    public double getLargo() {
+        return largo;
+    }
+
+    public void setLargo(double largo) {
+        this.largo = largo;
+    }
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(double ancho) {
+        this.ancho = ancho;
+    }
+    
+    
+}
